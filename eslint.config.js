c7e1468bb081df6ac4d8a3665c824edc08e49f22
Jeff Rescignano <jeff@jeffresc.dev>
2025-07-18 14:35:11 -0400

    lint fix

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+      'curly': 'warn',
+      'eqeqeq': 'warn',
+      'no-throw-literal': 'warn',
+      'semi': 'off',
+    },
+  },
+  {
+    ignores: ['out/**', 'dist/**', '**/*.d.ts'],
+  },
+];
